cae26b319f2422c33efe529573c2a35b08c20333
Unknown <jcmansillam@correo.url.edu.gt>
2017-10-26 19:58:16 -0600

    First Commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , labTittle: 'Laboratorio 4',description: 'Desarrollo de APIS' });
 });
 
 module.exports = router;
