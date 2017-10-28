75af2ed82385c9e68f877ac85cb5abc785da70dd
Unknown <jcmansillam@correo.url.edu.gt>
2017-10-28 13:52:34 -0600

    Last changes

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Laboratorio 4' , labTittle: 'Laboratorio 4',description: 'Desarrollo de APIS' });
 });
 
-router.post('/api/login', function(req,res,next){
-  const user = {id:3};
-  const token = jwt.sign({user}, 'secret_key');
-  res.json({
-    token: token
-  });
-}); 
-
 module.exports = router;
